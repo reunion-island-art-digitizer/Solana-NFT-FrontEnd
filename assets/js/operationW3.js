86b90360b080228501d87954bbdf98fe1bc34251
JulimanDev <JulimanDev0101@gmail.com>
2021-09-13 07:54:26 +0300

    getbalance from owner

--- assets/js/operationW3.js
+++ assets/js/operationW3.js
@@ -1,39 +1,42 @@
 // import web3 from 'web3'
-// window.addEventListener('load', function() {
+const totalAmount = 6666;
+let mintedAmount = 198;
 
+window.addEventListener('load', function() {
 
-//     // Load WEB3
-//     // Check wether it's already injected by something else (like Metamask or Parity Chrome plugin)
-//     if (typeof web3 !== 'undefined') {
-//         window.web3 = new Web3(web3.currentProvider);
+    $(".connectbtn").text("CONNECT");
+    $(".mintedAmunt").text(restAmount + '/' + totalAmount);
+    // Load WEB3
+    // Check wether it's already injected by something else (like Metamask or Parity Chrome plugin)
+    // if (typeof web3 !== 'undefined') {
+    //     window.web3 = new Web3(web3.currentProvider);
 
-//         // Or connect to a node
-//     } else {
-//         window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-//     }
+    //     // Or connect to a node
+    // } else {
+    //     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    // }
 
-//     // Check the connection
-//     // if (!window.web3.isConnected()) {
-//     //     console.error("Not connected");
+    // // Check the connection
+    // // if (!window.web3.isConnected()) {
+    // //     console.error("Not connected");
 
-//     // }
-//     window.web3.eth.getAccounts().then(result => { console.log(result) });
-//     // console.log(result);
-//     var accountInterval = setInterval(function() {
-//         if (web3.eth.accounts[0] !== account) {
-//             account = web3.eth.accounts[0];
-//             document.getElementById("address").innerHTML = account;
-//         }
-//     }, 100);
+    // // }
+    // window.web3.eth.getAccounts().then(result => { console.log(result) });
+    // // console.log(result);
+    // var accountInterval = setInterval(function() {
+    //     if (web3.eth.accounts[0] !== account) {
+    //         account = web3.eth.accounts[0];
+    //         document.getElementById("address").innerHTML = account;
+    //     }
+    // }, 100);
 
-// });
-$(".connectbtn").text("CONNECT");
+});
+
 
 $(".connectbtn").on("click", async function() {
     window.web3 = new Web3(window.web3.currentProvider);
     window.ethereum.enable();
     let address = await window.web3.eth.getAccounts();
-    console.log(address)
     let balance = await window.web3.eth.getBalance(address[0]);
     let result = balance / 1000000000000000000;
     $("#ethBalance").text(result);
@@ -41,6 +44,7 @@ $(".connectbtn").on("click", async function() {
 
 })
 $("#nftBuy").on("click", function() {
+
     let restAmount = 2000;
     let mintAmount = $("#mintAmount").val();
     let cost = 0.0017;
@@ -48,7 +52,9 @@ $("#nftBuy").on("click", function() {
         $("#nftBut").text("BUY");
         $("#nftBut").attr(onclick, "window.location.href='https://Opensea.io';");
         $("#mintAmount").val("0");
-        alert("SOLD OUT");
+        $(".mintedAmunt").text("SOLD OUT");
+
+
     } else {
         $("#nftBut").text("OPENSEA");
         $("#nftBut").removeAttr("onclick");
