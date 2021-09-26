773ed0f8b4b59a504e58ef5759b9f6af5e9d3ea9
Quentin Goujon <goujonquentin@hotmail.com>
2021-09-27 01:33:46 +0400

    r

--- assets/js/operationW3.js
+++ assets/js/operationW3.js
@@ -33,7 +33,7 @@ window.addEventListener('load', function() {
 });
 
 
-$(".connectbtn")
+$(".connectbtn").
     import { ConnectToPhantom } from "./phantom-wallet-example/components/ConnectToPhantom";
     export default function Home() {
         return (
