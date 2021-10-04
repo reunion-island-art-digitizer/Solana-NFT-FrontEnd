389e44ed52f2a0e7bf30b87a0deb8c3a770dcf3f
Quentin Goujon <goujonquentin@hotmail.com>
2021-10-04 16:16:46 +0400

    mise a jour connect bouton

--- assets/js/operationW3.js
+++ assets/js/operationW3.js
@@ -34,70 +34,16 @@ window.addEventListener('load', function() {
 
 
 $(".connectbtn").on("click", async function() {
-    import { useWalletModal } from "@solana/wallet-adapter-ant-design";
-    import { useWallet } from "@solana/wallet-adapter-react";
-    import { Button, Dropdown, Menu } from "antd";
-    import { ButtonProps } from "antd/lib/button";
-    import React, { useCallback } from "react";
-    import { LABELS } from "../../constants";
+    var web3 = require('@solana/web3.js');
+    var splToken = require('@solana/spl-token');
 
-    export interface ConnectButtonProps
-        extends ButtonProps,
-        React.RefAttributes<HTMLElement> {
-        allowWalletChange?: boolean;
-    }
-
-    export const ConnectButton = (props: ConnectButtonProps) => {
-        const { setVisible } = useWalletModal();
-
-        const { connected, connect, select, wallet, wallets } = useWallet();
-        const { onClick, children, disabled, allowWalletChange, ...rest } = props;
-
-        const handleChangeWalletButtonClick: React.MouseEventHandler<HTMLElement> = useCallback(
-            (event) => {
-                if (connected) {
-                    onClick?.(event);
-                    return;
-                }
-                setVisible(true);
-            },
-            [setVisible, onClick, connected]
+    (async () => {
+        // Connect to cluster
+        var connection = new web3.Connection(
+            web3.clusterApiUrl("devnet"),
+            'confirmed',
         );
-
-        // only show if wallet selected or user connected
-        const menu = (
-            <Menu>
-                {wallets.map((wallet) => (
-                    <Menu.Item key={wallet.name} onClick={() => select(wallet.name)}>
-                        Change Wallet to {wallet.name}
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-
-        if (!wallet || !allowWalletChange) {
-            return (
-                <Button
-                    {...rest}
-                    onClick={handleChangeWalletButtonClick}
-                    disabled={connected && disabled}
-                >
-                    {connected ? props.children : LABELS.CONNECT_LABEL}
-                </Button>
-            );
-        }
-
-        return (
-            <Dropdown.Button
-                onClick={connected ? onClick : connect}
-                disabled={connected && disabled}
-                overlay={menu}
-            >
-                {LABELS.CONNECT_LABEL}
-            </Dropdown.Button>
-        );
-    };
-
+    })
     $("#solBalance").text(result);
     $(".connectbtn").text("CONNECTED");
 
