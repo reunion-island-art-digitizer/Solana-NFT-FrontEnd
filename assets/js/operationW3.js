dd1b855672d9b8d28212d7343d691baf056b289b
Quentin Goujon <goujonquentin@hotmail.com>
2021-10-04 16:50:09 +0400

    s

--- assets/js/operationW3.js
+++ assets/js/operationW3.js
@@ -34,70 +34,22 @@ window.addEventListener('load', function() {
 
 
 $(".connectbtn").on("click", async function() {
-    import { useWalletModal } from "@solana/wallet-adapter-ant-design";
-    import { useWallet } from "@solana/wallet-adapter-react";
-    import { Button, Dropdown, Menu } from "antd";
-    import { ButtonProps } from "antd/lib/button";
-    import React, { useCallback } from "react";
-    import { LABELS } from "../../constants";
+    import React from "react";
+    import ReactDOM from "react-dom";
+    import "./src/index.css";
+    import App from "./src/App";
+    import * as serviceWorker from "./src/serviceWorker";
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
 
-    export interface ConnectButtonProps
-        extends ButtonProps,
-        React.RefAttributes<HTMLElement> {
-        allowWalletChange?: boolean;
-    }
-
-    export const ConnectButton = (props: ConnectButtonProps) => {
-        const { setVisible } = useWalletModal();
-
-        const { connected, connect, select, wallet, wallets } = useWallet();
-        const { onClick, children, disabled, allowWalletChange, ...rest } = props;
-
-        const handleChangeWalletButtonClick: React.MouseEventHandler<HTMLElement> = useCallback(
-            (event) => {
-                if (connected) {
-                    onClick?.(event);
-                    return;
-                }
-                setVisible(true);
-            },
-            [setVisible, onClick, connected]
-        );
-
-        // only show if wallet selected or user connected
-        const menu = (
-            <Menu>
-                {wallets.map((wallet) => (
-                    <Menu.Item key={wallet.name} onClick={() => select(wallet.name)}>
-                        Change Wallet to {wallet.name}
-                    </Menu.Item>
-                ))}
-            </Menu>
-        );
-
-        if (!wallet || !allowWalletChange) {
-            return (
-                <Button
-                    {...rest}
-                    onClick={handleChangeWalletButtonClick}
-                    disabled={connected && disabled}
-                >
-                    {connected ? props.children : LABELS.CONNECT_LABEL}
-                </Button>
-            );
-        }
-
-        return (
-            <Dropdown.Button
-                onClick={connected ? onClick : connect}
-                disabled={connected && disabled}
-                overlay={menu}
-            >
-                {LABELS.CONNECT_LABEL}
-            </Dropdown.Button>
-        );
-    };
-
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
     $("#solBalance").text(result);
     $(".connectbtn").text("CONNECTED");
 
