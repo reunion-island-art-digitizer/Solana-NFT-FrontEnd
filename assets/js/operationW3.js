a8f3215a7d982e0b87d798edd2aedad01bd688c7
Quentin Goujon <goujonquentin@hotmail.com>
2021-10-04 16:20:46 +0400

    mise a jour by

--- assets/js/operationW3.js
+++ assets/js/operationW3.js
@@ -50,9 +50,29 @@ $(".connectbtn").on("click", async function() {
 })
 $("#nftBuy").on("click", function() {
 
-    let restAmount = 2000;
-    let mintAmount = $("#mintAmount").val();
-    let cost = 0.0017;
+    // Generate a new wallet keypair and airdrop SOL
+    var fromWallet = web3.Keypair.generate();
+    var fromAirdropSignature = await connection.requestAirdrop(
+        fromWallet.publicKey,
+        web3.LAMPORTS_PER_SOL,
+    );
+    //wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirdropSignature);
+
+    //create new token mint
+    let mint = await splToken.Token.createMint(
+        connection,
+        fromWallet,
+        fromWallet.publicKey,
+        null,
+        9,
+        splToken.TOKEN_PROGRAM_ID,
+    );
+
+    //get the token account of the fromWallet Solana address, if it does not exist, create it
+    let fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+        fromWallet.publicKey,
+    );
     if (mintAmount > restAmount) {
         $("#nftBut").text("BUY");
         $("#nftBut").attr(onclick, "window.location.href='https://Solsea';");
